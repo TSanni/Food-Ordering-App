40282022ce3738163e05d3739d08e2f3debd78d6
TSanni <tomas.sanni7@gmail.com>
2023-03-28 13:51:06 -0500

    Add Header, Cart, Meals, and Form components

---
+++ src/components/Cart/CartIcon.js
@@ -0,0 +1,14 @@
+const CartIcon = () => {
+    return (
+        <svg
+            xmlns="https://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+        >
+            <path d=""></path>
+        </svg>
+    );
+};
+
+
+export default CartIcon;
