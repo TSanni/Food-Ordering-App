9bbe8c1c0988f2e4651d8fc0670ee0ac293ef5c9
TSanni <tomas.sanni7@gmail.com>
2023-03-29 04:03:12 -0500

    Add Header, Cart, Meals, and Form components

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import mealsImage from "../..assets/meals.jpg"
+import mealsImage from "../../assets/meals.jpg"
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
 //Remember to add image later 
@@ -11,7 +11,7 @@ const Header = (props) => {
             <HeaderCartButton />
         </header>
         <div className={classes["main-image"]}>
-            {/* <img src={mealsImage} alt="A table full of delicious food!"/> */}
+            <img src={mealsImage} alt="A table full of delicious food!"/>
         </div>
     </React.Fragment>
 
