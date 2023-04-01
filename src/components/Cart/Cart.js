ebbf8541f8d37838469c4de472d2e455bd149a5c
TSanni <tomas.sanni7@gmail.com>
2023-03-31 20:15:43 -0500

    Finish version one of Food Order

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -13,11 +13,11 @@ function Cart(props) {
     const hasItems = cartCtx.items.length > 0;
 
     const cartItemRemoveHandler = (id) => {
-
+        cartCtx.removeItem(id);
     };
 
     const cartItemAddHandler = (item) => {
-
+        cartCtx.addItem({...item, amount: 1});
     };
 
     const cartItems = <ul className={classes["cart-itmes"]}>{cartCtx.items.map(item => <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)}/>)}</ul>;
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import mealsImage from "../../assets/meals.jpg"
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
-//Remember to add image later 
+
 const Header = (props) => {
 
     return <React.Fragment>
